6679748b80934e61b8205c7cf34debce64eeb0f7
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2021-10-11 22:24:58 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import questionData from './data.json';
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  
+  // useEffect(()=>{
+  //   let data = questionData.data.questions.map((q)=>{return q});
+  //   setMainList(data);
+  // },[]);
+
+  const [value,setValue] = useState("");
+  const [fname,setFname] = useState("");
+  const [yesList,setYesList] = useState([]);
+  const [noList,setNoList] = useState([]);
+  const [answerListed,setAnswerList] = useState(false);
+
+  const handleChange = (event)=>{
+    setValue(event.target.value);
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setValue("");
+    setFname(value);
+  }
+
+  const handleSecondSubmit = (e)=>{
+    e.preventDefault();
+    setAnswerList(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>Name: <input type="text" value={value} onChange={handleChange} /></label>
+        <input type="submit" value="Submit" />
+      </form>
+      <div className="nameDisplay">
+        {fname && `Hey ! ${fname}`}
+      </div>
+      <div className="questionDisplay">
+        <form onSubmit={handleSecondSubmit}>
+          {
+            questionData.data.questions.map((ques)=>{
+              return(
+                <Card key={ques.id}>
+                 <span> {ques.question}</span>
+                 <div>
+                  <Button variant="outline-success" onClick={(e)=>{
+                    let yessList = [];
+                    yessList.push(ques.question);
+                    setYesList([...yesList,...yessList]);
+                  }} value="yes">Yes</Button>
+                  <Button variant="outline-danger" onClick={(e)=>{
+                    let nosList = [];
+                    nosList.push(ques.question);
+                    setNoList([...noList,...nosList]);
+                  }} value="no">No</Button>
+                </div> 
+                </Card>)
+            })
+          }
+          <input type="submit" value="Submit" />
+          <button value="reset" onClick={()=>{window.location.reload()}}>Reset</button>
+        </form>  
+      </div> 
+      <div className="listDisplay">
+        {answerListed &&(`Yes Answer Listed :${yesList}-----------------
+                          No Answer Listed :${noList}`)}
+      </div>  
     </div>
   );
 }
